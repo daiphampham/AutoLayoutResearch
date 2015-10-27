11afbd2d85e14953caf22c26ae335a6f25b6bf52
Quoc Nguyen <quocnb.vnu@gmail.com>
2015-10-27 08:20:01 +0700

    AutolayoutResearch_Simple

---
+++ AutoLayoutResearch/AutoLayoutResearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutResearch
+//
+//  Created by Quoc Nguyen on 10/27/15.
+//  Copyright © 2015 QuocNguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
